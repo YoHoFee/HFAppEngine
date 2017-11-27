232a194dd5e0074ebba26be47b75bde1e41c6041
姚鸿飞 <yohofee@hotmail.com>
2017-11-27 15:04:19 +0800

    - 修复BUG

---
+++ AppEngine/Configuration/API.swift
@@ -0,0 +1,53 @@
+//
+//  API.swift
+//  Ecosphere
+//
+//  Created by 姚鸿飞 on 2017/6/2.
+//  Copyright © 2017年 encifang. All rights reserved.
+//
+
+import UIKit
+
+class API: NSObject {
+
+    
+    // MARK: 服务器地址
+    static let baseURL          = "http://serve.yohofee.com/"
+    
+    
+    
+    
+    // MARK: - 通用API -
+    
+
+    
+    // MARK: 图片上传
+    static let imgUp            = ""
+    
+    
+    
+    
+    
+    // MARK: - 登录注册模块API -
+    
+    
+    
+    // MARK: 登陆
+    static let login            = ""
+    
+    // MARK: 注册
+    static let register         = ""
+    
+    // MARK: 刷新Token
+    static let TimeToken        = ""
+    
+    // MARK: 自动登录
+    static let autoLogin        = ""
+    
+    // MARK: 忘记密码
+    static let ForgetPwd        = ""
+    
+    
+    
+    
+}
