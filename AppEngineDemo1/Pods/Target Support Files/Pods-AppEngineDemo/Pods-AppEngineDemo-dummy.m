5af44d6f0277cb62ad775ecec64b0697f61ae247
姚鸿飞 <yohofee@hotmail.com>
2017-11-27 15:06:21 +0800

    - 修复BUG

--- AppEngineDemo1/Pods/Target Support Files/Pods-AppEngineDemo/Pods-AppEngineDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AppEngineDemo : NSObject
-@end
-@implementation PodsDummy_Pods_AppEngineDemo
-@end
--- AppEngineDemo1/Pods/Target Support Files/Pods-AppEngineDemo/Pods-AppEngineDemo-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_AppEngineDemoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_AppEngineDemoVersionString[];
-
