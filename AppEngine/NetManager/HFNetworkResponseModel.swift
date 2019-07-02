89d4927d1c37de2f942595deb77845bec001045a
姚鸿飞 <yohofee@hotmail.com>
2019-07-02 18:43:52 +0800

    - 添加注释

--- AppEngine/NetManager/HFNetworkResponseModel.swift
+++ AppEngine/NetManager/HFNetworkResponseModel.swift
@@ -12,10 +12,20 @@ import SwiftyJSON
 
 class HFNetworkResponseModel: NSObject {
 
+    
+    /// 请求状态
     var status: Int?
+    
+    /// 服务器消息
     var msg: String?
+    
+    /// 数据包
     var data: JSON?
+    
+    /// 请求码
     var code: Int?
+    
+    /// 原始数据包
     var rawData: JSON?
     
     init(status: Int?, msg: String?, data:JSON?, code: Int?,rawData: JSON?) {
